37e9adc27fbe83ec287aed73113c9bd3b44999af
Sid Ngeth <sidn@tripleseat.com>
2023-11-15 23:43:27 -0500

    Update index page

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::fs::{FileServer, NamedFile};
+use rocket::form::Form;
+use rocket::response::content::RawHtml;
+
+
+#[derive(FromForm)]
+struct Exercise {
+    exercise: String,
+    weight: String,
 }
 
+#[post("/submit-exercise", data = "<exercise_form>")]
+async fn submit_exercise(exercise_form: Form<Exercise>) -> RawHtml<String> {
+    let exercise = exercise_form.into_inner();
+    RawHtml(format!("<p>Exercise: {} - Weight: {}</p>", exercise.exercise, exercise.weight))
+}
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open("static/index.html").await.ok()
+}
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, submit_exercise])
+        .mount("/static", FileServer::from("static"))
 }
